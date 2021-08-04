daa36e2d16522b5fa95ab7ddc0c76713adbbf9d3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-03 18:09:27 -0700

    KeyStroke_V1.py
    An archive of the first version of the KeyStroke program.

---
+++ KeyStroke/Stroke/OldVersions/1/1-100/KeyStroke_V1.py
@@ -0,0 +1,67 @@
+# Start of script
+# Import section
+import random
+# Integer section
+x = int(1)
+# Function definitions section
+def key():
+  keyX = int(35) # Every x keystrokes, a stroke occurs
+  keyY = int(35) # For every x Keystrokes, there are y characters in the stroke
+  break
+def wait():
+  # Method incomplete
+  # How do I work with time in Python?
+  break
+def stroke():
+  for key in range(x, y):
+    wait()
+    return text(strokeR) # Invalid syntax as pseudocode, it should start writing in the current open object
+  break
+def strokeR(): # Stroke randomizer
+  r = random.randint(1, 20)
+  if (r == 1):
+    strokeR = str("NSJJFJSDHFHJDKJFKSHJFHSKJFHGJDHF")
+  else:
+    strokeR = str("ABABSDJHDKJSHKDHKDJDKDJSKDKLSKDL")
+  # I need a better method of randomizing strings
+def changeStroke():
+  # For adjusting stroke settings
+  changeConfirm = input("Do you want to change the stroke settings? y/n")
+  changeConfirm == changeConfirm.upper()
+  if (changeConfirm == y):
+    print ("Every " + keyX + " keystrokes, a stroke occurs")
+    changeX = int(input("Change to: "))
+    if (changeX == int and changeX > 1):
+      keyX == changeX
+    print ("For every " + keyX + " keystrokesd, there are " + keyY + " characters in the stroke")
+    changeY = int(input("Change to: "))
+    if (changeY == int and changeY > 1):
+      keyY == changeY
+   else:
+    print ("Canceling")
+def main():
+  print ("Keystroke")
+  print ("Keystroke is a program that makes your keyboard have a stroke every once in a whileNSJJFJSDHFHJDKJFKSHJFHSKJFHGJDHF")
+  mainInput = input("Change settings (cs) test (t) quit (q)")
+  mainInput == mainInput.upper()
+  if (mainInput == "CS"):
+    return changeStroke()
+  if (mainInput == "T"):
+    return stroke()
+  if (mainInput == "Q"):
+    return nil
+    break
+    crash.end() # Invalid syntax to close the program
+# The program
+while (x == x):
+  return main()
+  return wait()
+  break
+print ("The program has crashed")
+break
+""" File info
+File type: Python source file (*.py)
+File version: 1 (Tuesday, August 3rd 2021 at 6:09 pm)
+Line count (including blank lines and compiler line): 68
+"""
+# End of script
